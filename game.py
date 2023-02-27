5d5a6531f808250ce5e74909a04e4a872bff7bed
Mizakson <maxwellizakson@gmail.com>
2023-02-27 12:03:55 -0500

    git commit

--- game.py
+++ game.py
@@ -3,3 +3,16 @@ from player import Player
 from enemies import *
 import tkinter
 from tkinter import messagebox
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 576
+
+# Define colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+RED = (255,0,0)
+
+class Game(object):
+    def __init__(self):
+        self.font = pygame.font.Font(name, size)
+
