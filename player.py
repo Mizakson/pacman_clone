2cd3043c9dda2e9c0d4a7a803491099bad270743
Mizakson <maxwellizakson@gmail.com>
2023-02-28 13:29:17 -0500

    git commit -m "finished player.py"

--- player.py
+++ player.py
@@ -159,3 +159,30 @@ class Animation(object):
 
         # copy the sprite from the large sheet onto the smaller one
         image.blit(self.sprite_sheet, (0,0),(x,y,width,height))
+
+        # making black the transparent color
+        image.set_colorkey((0,0,0))
+
+        return image
+
+
+    def get_current_image(self):
+        return self.image_list[self.index]
+
+    def get_length(self):
+        return len(self.image_list)
+
+    def update(self,fps=30):
+        step = 30 // fps
+        # FPS (Frames Per Second limit) 30
+        l = range(1,30,step)
+        if self.clock == 30:
+            self.clock = 1
+        else:
+            self.clock += 1
+
+        if self.clock in l:
+            # increase the index
+            self.index += 1
+            if self.index == len(self.image_list):
+                self.index = 0
