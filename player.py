d4b74c4532651fb6f1d65c8bec2c6f8c85c38146
Mizakson <maxwellizakson@gmail.com>
2023-02-28 09:45:52 -0500

    git commit -m "created player.py /n imported neccessary modules and defined constants"

---
+++ player.py
@@ -0,0 +1,13 @@
+# import necessary modules
+import pygame
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 576
+
+#define colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+# Player class creation
+class Player(pygame.sprite.Sprite):
+    pass
